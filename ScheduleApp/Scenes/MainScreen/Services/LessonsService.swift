52e7207c23d81d7c598b7007968ddb823ae25f87
Igor <klyuzhevigor@gmail.com>
2022-09-26 00:50:18 +0300

    SCHED-6: [refactor] Used alamofire authenticate

--- ScheduleApp/Scenes/MainScreen/Services/LessonsService.swift
+++ ScheduleApp/Scenes/MainScreen/Services/LessonsService.swift
@@ -10,13 +10,11 @@ import Alamofire
 
 final class LessonsService {
 	func getLessons() async throws -> [LessonModel] {
-		let authstr = "admin:admin"
-		let auth = Data(authstr.utf8).base64EncodedString()
 		let decoder = JSONDecoder()
 		decoder.dateDecodingStrategy = .iso8601
-		return try await AF.request(Constants.Network.baseUrl + "/lesson",
-									headers: ["Authorization": "Basic \(auth)"])
-						   .serializingDecodable([LessonModel].self, decoder: decoder)
-						   .value
+		return try await AF.request(Constants.Network.baseUrl + "/lesson")
+			.authenticate(username: "admin", password: "admin")
+			.serializingDecodable([LessonModel].self, decoder: decoder)
+			.value
 	}
 }
